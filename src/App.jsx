38f0d11ebdc7e456c84c146b03314989758b4cc6
mathgui30 <matheus.gdepaula@gmail.com>
2025-05-12 22:08:59 -0300

    implementação da pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,162 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import { useState } from 'react';
 import './App.css'
+import pokeballFoto from './assets/pokeball.png'
+import PokemonCard from './components/Pokemoncard';
+
+
+function PokeballIcon() {
+  return (
+    <svg
+      className="pokeball-icon"
+      viewBox="-30 -30 600 600"
+      width="24"
+      height="24"
+    >
+      {/* Outer circle filled black */}
+      <circle
+        className='outline'
+        cx="256"
+        cy="256"
+        r="260"
+        fill="#000"
+        stroke='#000'
+        strokeWidth="32" />
+
+      {/* Bottom half inside outer circle filled white */}
+      <path
+        d="M16,256 A240,240 0 0,0 496,256 496,496 L16,496 Z"
+        fill="#fff"
+        stroke="#000"
+        strokeWidth="7"
+      />
+
+      {/* Center circle */}
+      <circle
+        cx="256"
+        cy="256"
+        r="95"
+        fill="#fff"
+        stroke="#000"
+        strokeWidth="30"
+      />
+    </svg>
+  );
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
+  const [initialPokemons, setInitialPokemons] = useState([])
+  const [isInitialLoading ,setIsInitialLoading] = useState(true)
+  const [listError, setListError] = useState(null);
+
+  useEffect (() =>  {
+    fetchPokemonList();
+  }, [])
+
+  const fetchPokemonList = async () => {
+    try {
+      const listResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      if (!listResponse.ok) throw new Error('Falha ao buscar lista');
+      const listData = await listResponse.json();
+
+      const detailedPokemons = await Promise.allSettled(
+        listData.results.map(async (pokemon) => {
+          const res = await fetch(pokemon.url);
+          if (!res.ok) throw new Error('Falha no Pokémon individual');
+          return await res.json();
+        })
+      );
+
+      const successfulPokemons = detailedPokemons
+        .filter(result => result.status === 'fulfilled')
+        .map(result => result.value)
+        .sort((a, b) => a.id - b.id);
+
+      setInitialPokemons(successfulPokemons);
+      setListError(null);
+    } catch (error) {
+      setListError(error.message);
+      console.error('Erro na busca:', error);
+    } finally {
+      setIsInitialLoading(false);
+    }
+  };
+
+  const handleSearch = () => {
+    setQuery(search.trim())
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  }
+
+  const handleClear = () => {
+    setSearch('')
+    setQuery('')
+  }
+
+  const listRef = useRef(null)
+
+  const handleWheel = (e) => {
+    if(listRef.current) {
+      e.preventDefault()
+
+      const scrollAmount = e.deltaY * 2
+
+
+      listRef.current.scrollLeft += scrollAmount
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="Pokedex">
+      <div className="search-container">
+        <img src={pokeballFoto} alt="Background"
+          className="background-image" />
+        <input
+          type="text"
+          className='search-bar'
+          placeholder='Pesquise seu pokémon'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={handleKeyDown}
+
+        />
+        <button
+          className="search-button"
+          aria-label='Search'
+          onClick={handleClear}>
+          <PokeballIcon />
+
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {!query && (
+      <div 
+      className="horizontal-list"
+      ref={listRef}
+      onWheel={handleWheel}
+      >
+        {initialPokemons.map(pokemon => (
+          <PokemonCard 
+            key={pokemon.id}
+            name={pokemon.name}
+            compact
+          />
+        ))}
+      </div>)}
+
+
+      <div className="card-container">
+        {query && <PokemonCard name={query} />}
+      </div>
+    </div>
   )
 }
 
